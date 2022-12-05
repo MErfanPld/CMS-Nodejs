b1a715a88f4cdfa140e5c36427751af05944d651
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2022-12-05 20:10:26 +0330

    Create Search Box

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,7 @@ const Blog = require("../models/Blog");
 const multer = require("multer");
 const sharp = require("sharp");
 const shortId = require("shortid");
-const fs = require('fs')
+const fs = require("fs");
 
 const { formatDate } = require("../utils/jalali");
 const { storage, fileFilter } = require("../utils/multer");
@@ -127,65 +127,64 @@ exports.editPost = async (req, res) => {
 
   const post = await Blog.findOne({ _id: req.params.id });
   try {
-      if (thumbnail.name)
-          await Blog.postValidation({ ...req.body, thumbnail });
-      else
-          await Blog.postValidation({
-              ...req.body,
-              thumbnail: {
-                  name: "placeholder",
-                  size: 0,
-                  mimetype: "image/jpeg",
-              },
-          });
+    if (thumbnail.name) await Blog.postValidation({ ...req.body, thumbnail });
+    else
+      await Blog.postValidation({
+        ...req.body,
+        thumbnail: {
+          name: "placeholder",
+          size: 0,
+          mimetype: "image/jpeg",
+        },
+      });
 
-      if (!post) {
-          return res.redirect("errors/404");
-      }
+    if (!post) {
+      return res.redirect("errors/404");
+    }
 
-      if (post.user.toString() != req.user._id) {
-          return res.redirect("/dashboard");
-      } else {
-          if (thumbnail.name) {
-              fs.unlink(
-                  `${appRoot}/public/uploads/thumbnails/${post.thumbnail}`,
-                  async (err) => {
-                      if (err) console.log(err);
-                      else {
-                          await sharp(thumbnail.data)
-                              .jpeg({ quality: 60 })
-                              .toFile(uploadPath)
-                              .catch((err) => console.log(err));
-                      }
-                  }
-              );
+    if (post.user.toString() != req.user._id) {
+      return res.redirect("/dashboard");
+    } else {
+      if (thumbnail.name) {
+        fs.unlink(
+          `${appRoot}/public/uploads/thumbnails/${post.thumbnail}`,
+          async (err) => {
+            if (err) console.log(err);
+            else {
+              await sharp(thumbnail.data)
+                .jpeg({ quality: 60 })
+                .toFile(uploadPath)
+                .catch((err) => console.log(err));
+            }
           }
-
-          const { title, status, body } = req.body;
-          post.title = title;
-          post.status = status;
-          post.body = body;
-          post.thumbnail = thumbnail.name ? fileName : post.thumbnail;
-
-          await post.save();
-          return res.redirect("/dashboard");
+        );
       }
+
+      const { title, status, body } = req.body;
+      post.title = title;
+      post.status = status;
+      post.body = body;
+      post.thumbnail = thumbnail.name ? fileName : post.thumbnail;
+
+      await post.save();
+      return res.redirect("/dashboard");
+    }
   } catch (err) {
-      console.log(err);
-      err.inner.forEach((e) => {
-          errorArr.push({
-              name: e.path,
-              message: e.message,
-          });
+    console.log(err);
+    err.inner.forEach((e) => {
+      errorArr.push({
+        name: e.path,
+        message: e.message,
       });
-      res.render("admin/editPost", {
-          pageTitle: "بخش مدیریت | ویرایش پست",
-          path: "/dashboard/edit-post",
-          layout: "./layouts/adminLayout",
-          fullname: req.user.fullname,
-          errors: errorArr,
-          post,
-      });
+    });
+    res.render("admin/editPost", {
+      pageTitle: "بخش مدیریت | ویرایش پست",
+      path: "/dashboard/edit-post",
+      layout: "./layouts/adminLayout",
+      fullname: req.user.fullname,
+      errors: errorArr,
+      post,
+    });
   }
 };
 
@@ -234,4 +233,40 @@ exports.uploadImage = (req, res) => {
       }
     }
   });
+};
+
+exports.handleDashSearch = async (req, res) => {
+  const page = +req.query.page || 1;
+  const postPerPage = 2;
+
+  try {
+      const numberOfPosts = await Blog.find({
+          user: req.user._id,
+          $text: { $search: req.body.search },
+      }).countDocuments();
+      const blogs = await Blog.find({
+          user: req.user.id,
+          $text: { $search: req.body.search },
+      })
+          .skip((page - 1) * postPerPage)
+          .limit(postPerPage);
+
+      res.render("admin/blogs", {
+          pageTitle: "بخش مدیریت | داشبورد",
+          path: "/dashboard",
+          layout: "./layouts/adminLayout",
+          fullname: req.user.fullname,
+          blogs,
+          formatDate,
+          currentPage: page,
+          nextPage: page + 1,
+          previousPage: page - 1,
+          hasNextPage: postPerPage * page < numberOfPosts,
+          hasPreviousPage: page > 1,
+          lastPage: Math.ceil(numberOfPosts / postPerPage),
+      });
+  } catch (err) {
+      console.log(err);
+      get500(req, res);
+  }
 };
